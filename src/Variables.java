ba60593d04caa29fae7397c40cf8241e5f17fa21
AijanAbaskanova <abaskanova@gmail.com>
2025-02-16 21:06:49 -0600

    Added variables code

---
+++ src/Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        byte number = 5;
+        short price = 356;
+        int age = 38;
+        long population = 74939485;
+        double salaryPerHour = 25.50;
+        float pi = 3.14f;
+        boolean growthHungryStudent= true;
+        char letter = 'a';
+        String name = "Aijan";
+
+        System.out.println("My name is " + name + ", I am " + age + "years old");
+        System.out.println("Population of city is " + population);
+        System.out.println("My salary is " + salaryPerHour);
+
+
+    }
+}
