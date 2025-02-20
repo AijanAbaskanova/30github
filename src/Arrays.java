f18e9aee2ab9469cdd21609902c84e14abb9c42f
AijanAbaskanova <abaskanova@gmail.com>
2025-02-19 22:02:36 -0600

    Added arrays

---
+++ src/Arrays.java
@@ -0,0 +1,13 @@
+public class Arrays {
+    public static void main(String[] args) {
+
+        // Creating and initializing an array
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Accessing elements of the array
+        System.out.println("Array elements:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+    }
+}
