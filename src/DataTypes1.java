59497fe9efdf52d0963c6e3de1a10b7616ec7e57
AijanAbaskanova <abaskanova@gmail.com>
2025-02-22 21:12:09 -0600

    Data Types

---
+++ src/DataTypes1.java
@@ -0,0 +1,29 @@
+public class DataTypes1 {
+    public static void main(String[] args) {
+        // Primitive Data Types
+        byte byteVar = 127;
+        short shortVar = 32000;
+        int intVar = 100000;
+        long longVar = 10000000000L;
+        float floatVar = 10.99f;
+        double doubleVar = 99.99;
+        char charVar = 'A';
+        boolean boolVar = true;
+
+        // Performing a simple operation using an int data type
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        // Printing the values
+        System.out.println("Byte: " + byteVar);
+        System.out.println("Short: " + shortVar);
+        System.out.println("Integer: " + intVar);
+        System.out.println("Long: " + longVar);
+        System.out.println("Float: " + floatVar);
+        System.out.println("Double: " + doubleVar);
+        System.out.println("Character: " + charVar);
+        System.out.println("Boolean: " + boolVar);
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
